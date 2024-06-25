e5f07defe2404537eae827c1a446ae3cb14c0c66
Frost <jaydanieldp18@gmail.com>
2024-06-26 02:13:36 +0800

    Popup

--- JS/home.js
+++ JS/home.js
@@ -17,3 +17,44 @@ document.getElementById('openPDFButton').addEventListener('click', function() {
   
   window.open(pdfUrl, '_blank');
 });
+
+const contactButton = document.getElementById('contactButton');
+const contactPopup = document.getElementById('contactPopup');
+const footer = document.querySelector('footer');
+
+let isPopupVisible = false;
+
+contactButton.addEventListener('click', function() {
+  if (isPopupVisible) {
+    contactPopup.classList.add('hide');
+    contactPopup.classList.remove('show');
+
+    
+    setTimeout(function() {
+      contactPopup.style.display = 'none';
+    }, 300); 
+  } else {
+    contactPopup.style.display = 'block';
+    contactPopup.classList.add('show');
+    contactPopup.classList.remove('hide');
+  }
+
+  isPopupVisible = !isPopupVisible;
+
+  setTimeout(function() {
+    if (isPopupVisible) {
+      contactPopup.classList.add('hide');
+      contactPopup.classList.remove('show');
+      isPopupVisible = false;
+
+      
+      setTimeout(function() {
+        contactPopup.style.display = 'none';
+      }, 300); 
+    }
+  }, 3000); 
+});
+
+contactPopup.addEventListener('click', function() {
+  footer.scrollIntoView({ behavior: 'smooth' });
+});
